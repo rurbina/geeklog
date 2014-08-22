198451860a252dbab3a1d5c5b3cdb1fbdf8e9a3d
Ricardo Rat Urbina <rat@sdf.org>
2014-08-22 17:55:55 -0500

    Básicamente nada más arreglé rmtest para convertirlo en un procesador command line. Esto simplifica bastante debuggear la interacción geeklog<->ratamarkup

--- rmtest.rkt
+++ rmtest.rkt
@@ -2,11 +2,20 @@
 
 (require "geeklog.rkt")
 (require "ratamarkup.rkt")
+(require racket/format)
 
 (geeklog-settings (make-hash (list
                        '(data-path . "/home/rat/Dropbox/GeekMX/")
                        '(suffixes . (".txt")))))
 
-(define mydoc (geeklog-load-doc "sandbox"))
+(map 
+ (lambda (item) (printf "[31m==> item [m\n~a\n" item))
+ (let ([files (vector->list (current-command-line-arguments))])
+   (when (empty? files) (set! files '("index")))
+   (map
+    (lambda (file)
+      (ratamarkup
+       (gldoc-body
+        (geeklog-load-doc file))))
+    files)))
 
-(display (format "~a\n" (ratamarkup (gldoc-body mydoc))))
