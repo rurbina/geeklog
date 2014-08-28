15aa2a10caef07cd2040fcc84c523017911794bb
Ricardo Rat Urbina <rat@sdf.org>
2014-08-28 14:10:23 -0500

    Ahora rmtest no llama a ratamarkup, sino que usa la API de geeklog exclusivamente.

--- rmtest.rkt
+++ rmtest.rkt
@@ -1,21 +1,14 @@
 #lang racket
 
 (require "geeklog.rkt")
-(require "ratamarkup.rkt")
 (require racket/format)
 
-(geeklog-settings (make-hash (list
-                       '(data-path . "/home/rat/Dropbox/GeekMX/")
-                       '(suffixes . (".txt")))))
+(geeklog-merge-settings (make-hash (list
+                                    '(data-path . "/home/rat/Dropbox/GeekMX/"))))
 
 (map 
- (lambda (item) (printf "[31m==> item [m\n~a\n" item))
+ (lambda (item) (printf "[31m== item [m\n~a\n" item))
  (let ([files (vector->list (current-command-line-arguments))])
    (when (empty? files) (set! files '("index")))
-   (map
-    (lambda (file)
-      (ratamarkup
-       (gldoc-body
-        (geeklog-load-doc file))))
-    files)))
+   (map (lambda (file) (gldoc-body (geeklog-load-doc file))) files)))
 
