858210d9b6524606dc50556d15493e825434b2f7
Ricardo Rat Urbina <rat@sdf.org>
2014-09-11 17:49:41 -0500

    Cambié un poco la estructura porque no necesito declarar un hash `doc` dummy

--- launch.rkt
+++ launch.rkt
@@ -3,19 +3,18 @@
 ; sample launch file for geeklog
 (require "geeklog.rkt")
 
-(let ([doc (make-hash '([title . placeholder]
-                        [body . placeholder]))])
-  (geeklog-merge-settings
-   (make-hash `([base-path . "geekmx"]
-                [data-path . "geekmx"]))
-   "geekmx.rkt")
+(geeklog-merge-settings
+ (make-hash `([base-path . "/www/geekmx"]
+              [data-path . "data"]
+              [template  . "geekmx.html"]))
+ "geekmx.rkt")
 
-  (geeklog-merge-settings 
-   (make-hash `([base-path . "/www/96grados"]
-                [data-path . "docs"]
-                [suffixes  . (".txt" ".link" "")]
-                [template  . "96grados.s-html"]))
-   "96g.rkt"))
+(geeklog-merge-settings 
+ (make-hash `([base-path . "/www/96grados"]
+              [data-path . "docs"]
+              [suffixes  . (".txt" ".link" "")]
+              [template  . "96grados.s-html"]))
+ "96g.rkt")
 
 (geeklog #:path "/www"
          #:data-path "omg")
