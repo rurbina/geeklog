208e079ecccffb83cfbc1bb29e1f721bc8d8eb4c
Ricardo Rat Urbina <rat@sdf.org>
2014-10-28 11:56:05 -0600

    Soporte para \vert en tablas de §orgtbl

--- geeklog.rkt
+++ geeklog.rkt
@@ -172,7 +172,7 @@
                    (html-table-row (for/list ([cell-list row-cells-list])
                                      (html-table-cell
                                       (ratamarkup-process
-                                       (string-join cell-list "\n")
+                                       (regexp-replace* #px"(?<!\\)\\vert" (string-join cell-list "\n") "|")
                                        #:options options))))))
                ;; and this one for normal row-per-line tables
                (for/list ([line (string-split text "\n")]
