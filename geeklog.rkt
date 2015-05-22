880aaa40c15369a5172e872fad14d721088b3603
Ricardo Rat Urbina <rat@sdf.org>
2015-05-22 16:59:33 -0500

    .

--- geeklog.rkt
+++ geeklog.rkt
@@ -295,20 +295,22 @@
   (let ([opts (hashify-tokens tokens
                               #:defaults '([width  "200"]
                                            [height "350"]
-                                           [album "1"]
+                                           [type "album"]
+                                           [id "1"]
                                            [bgcol "333333"]
                                            [url ""]
                                            [title ""]
                                            [style  "float:right; margin:4px"]))])
-    (format (string-append "<iframe style=\"~a; border: 0; width: ~apx; height: ~apx;\" src=\"https://bandcamp.com/EmbeddedPlayer/album=~a/size=large/bgcol=~a/linkcol=0f91ff/tracklist=false/transparent=true/\" seamless>"
+    (format (string-append "<iframe style=\"~a; border: 0; width: ~apx; height: ~apx;\" src=\"https://bandcamp.com/EmbeddedPlayer/~a=~a/size=large/bgcol=~a/linkcol=0f91ff/tracklist=false/transparent=true/\" seamless>"
                            "<a href=\"~a\">~a</a>"
                            "</iframe>")
             (hash-ref opts 'style "")
             (hash-ref opts 'width  "200")
             (hash-ref opts 'height "350")
-            (hash-ref opts 'album  "1")
+            (hash-ref opts 'type   "album")
+            (hash-ref opts 'id  "1")
             (hash-ref opts 'bgcol  "333333")
-            (hash-ref opts 'url    "bandcamp album url")
+            (hash-ref opts 'url    "bandcamp item url")
             (hash-ref opts 'title  "link title"))))
 
 (define (rm-div text
