b1e5c836962761e784a0dabd64638d1e09d073a3
Ricardo Urbina <ricardo.urbina@gmail.com>
2019-05-07 18:34:39 -0500

    Una tablita para cache pero le faltan cosas, de heco creo que voy a matar el cache

--- handlers.rkt
+++ handlers.rkt
@@ -6,13 +6,36 @@
 
 (struct gldoc (headers body))
 
-;; print out some server info
-(define (handler-cache req path headers settings)
-  (format "<html><head><title>Cached</title></head><body><h1>Server info</h1><h2>Cache dump</h2>~a</body></html>\n"
-          (string-join
-           (for/list ([key (hash-keys (hash-ref settings 'cache null))])
-             (format "<p><b>~a</b></p>\n" key)))))
+(define (render-cell cell) (format "  <td>~a</td>\n" cell))
 
+(define (render-row cells)
+  (string-join 
+   (list " <tr>\n"
+         (string-join (for/list ([cell cells]) (render-cell cell)))
+         " </tr>\n")))
+
+(define (render-table rows)
+  (string-join
+   (list "<table>\n"
+         (string-join (for/list ([row rows]) (render-row row)))
+         "</table>\n")))
+
+
+;; print out cached doc table
+(define (handler-cache req path headers settings)
+  (format
+   "<html><head><title>Cached</title></head><body><h1>Server info</h1><h2>Cache dump</h2>~a</body></html>\n"
+   (render-table
+    (append (list)
+            (list (list "Documento" "Fecha" "Tags" "Categorias"))
+            (for/list ([key (hash-keys (hash-ref settings 'cache null))])
+              (let ([headers (vector-ref (struct->vector (hash-ref (hash-ref settings 'cache) key)) 0)])
+                (list (format "~a" key)
+                      (format "~a" (hash-ref headers 'mtime))
+                      null
+                      null)))))))
+
+;; print out categories
 (define (handler-categories req path headers settings)
   (let ([cats (make-hash)]
         [headers (make-hash '((title "Categories")))]
