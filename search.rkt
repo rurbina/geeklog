b77c7d89e32473f51954e21f18d5ec6ec7aa5901
Ricardo Urbina <ricardo.urbina@gmail.com>
2019-07-03 22:35:53 -0500

    Le metí rango a la búsqueda, estamos un paso más cerca de poder paginar

--- search.rkt
+++ search.rkt
@@ -7,17 +7,21 @@
 
 ;; search for documents
 (define (search-docs
+         #:settings        settings
          #:tags            [tags         '()]
          #:or-tags         [or-tags      '()]
          #:no-tags         [no-tags      '()]
          #:and-tags        [and-tags     '()]
-         #:headers-only    [headers-only #t]
+         #:headers-only    [headers-only #f]
+         #:summary-only    [summary-only #t]
          #:sort            [sort-key     'name]
          #:reverse         [sort-reverse #f]
          #:no-future       [no-future    #f]
          #:older-than      [before-secs  0]
          #:newer-than      [after-secs   0]
-         #:settings        [settings     (make-hash)]
+         #:unparsed        [unparsed #f]
+         #:range-start     [range-start 0]
+         #:range-count     [range-count 10]
          #:data-path       [data-path    null])
   (when (null? data-path)
     (set! data-path (string->path (string-append (hash-ref settings 'base-path) "/" (hash-ref settings 'data-path)))))
@@ -30,11 +34,11 @@
                             (with-handlers ([exn:fail? (lambda (e)
                                                          (eprintf "\t\terror is ~v\n" e)
                                                          void)])
-                              (eprintf "	\e[33msearch: \e[0m~v\n" (path->string file-path))
+                              ;(eprintf "	\e[33msearch: \e[0m~v\n" (path->string file-path))
                               (load-doc (path->string (path-replace-extension (last (explode-path file-path)) #""))
                                         #:path file-path
                                         #:settings settings
-                                        #:headers-only headers-only)))]
+                                        #:headers-only #t)))]
                      #:when (and [gldoc? doc]
                                  [or (empty? tags)
                                      (subset? tags (hash-ref (gldoc-headers doc) 'tags '()))]
@@ -52,7 +56,22 @@
                                      (> (hash-ref (gldoc-headers doc) 'timestamp after-secs) after-secs)]
                                  #t))
             doc))
-    ;; sorting
+    ;; sort and trim range
     (set! results (sort results (lambda (a b) (gldoc-sort a b sort-key))))
-    (if sort-reverse (reverse results) results)))
-
+    (eprintf "  sort-reverse: ~v\n" sort-reverse)
+    (when sort-reverse (set! results (reverse results)))
+    (set! results (cond ((< (length results) range-start)
+                         '())
+                        (else
+                         (list-tail results range-start))))
+    (set! results (cond ((< (length results) range-count) results)
+                        (else (take results range-count))))
+    ;; load and process actual data
+    (unless headers-only
+      (set! results
+            (for/list ([doc results])
+              (load-doc null
+                        #:path (hash-ref (gldoc-headers doc) 'path)
+                        #:unparsed unparsed
+                        #:settings settings))))
+    results))
